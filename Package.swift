30c6cbd7601c79ab9c2ef7fbae9aa7bd42c11aa4
Yuya Oka <nnsnodnb@gmail.com>
2019-10-01 14:43:06 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Feeder",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "Feeder", targets: ["Feeder"])
     ],
